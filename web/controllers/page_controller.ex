4d29b8721638b4a285d4275613ea880e05ccb99b
Syed Murtza <syed.murtza@gmail.com>
2016-06-20 22:59:11 +0500

    Initial Commit

---
+++ web/controllers/page_controller.ex
@@ -0,0 +1,7 @@
+defmodule Cru.PageController do
+  use Cru.Web, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+end
---
+++ web/router.ex
@@ -0,0 +1,26 @@
+defmodule Cru.Router do
+  use Cru.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Cru do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Cru do
+  #   pipe_through :api
+  # end
+end
