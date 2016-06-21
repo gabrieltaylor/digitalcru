b299057b44e5de5055cffc1cd6bca1ecaf3de72d
Syed Murtza <syed.murtza@gmail.com>
2016-06-21 22:22:53 +0500

    add fontawesome and service model

---
+++ web/models/service.ex
@@ -0,0 +1,20 @@
+defmodule Cru.Service do
+  use Cru.Web, :model
+
+  schema "services" do
+    field :icon, :string
+    field :title, :string
+    field :body, :string
+
+    timestamps
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:icon, :title, :body])
+    |> validate_required([:icon, :title, :body])
+  end
+end
